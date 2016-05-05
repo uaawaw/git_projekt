4e342ef8b1c3357a0f724505579a13a21164d1a8
awaw <ruawaw@yandex.ru>
2016-05-05 21:09:19 +0300

    min and max

---
+++ max_min.py
@@ -0,0 +1,16 @@
+a1 = int(input())
+a2 = int(input())
+a3 = int(input())
+
+if (a1>=a2 and a1>=a3) and (a2>=a3):
+    print ('\n',a1,'\n',a3,'\n',a2)
+elif (a1>=a2 and a1>=a3) and (a3>=a2):
+    print ('\n',a1,'\n',a2,'\n',a3)
+elif (a2>=a1 and a2>=a3) and (a1>=a3):
+    print ('\n',a2,'\n',a3,'\n',a1)
+elif (a2>=a1 and a2>=a3) and (a3>=a1):
+    print ('\n',a2,'\n',a1,'\n',a3)
+elif (a3>=a1 and a3>=a2) and (a2>=a1):
+    print ('\n',a3,'\n',a1,'\n',a2)
+elif (a3>=a1 and a3>=a2) and (a1>=a2):
+    print ('\n',a3,'\n',a2,'\n',a1)
